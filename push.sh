3cc5e060277972cf36baa241d17bcb896e762d37
Giovanni Pecoraro <g.pecoraro@rheagroup.com>
2023-06-21 11:33:30 +0200

    Added CD pipeline

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo " [+] Logging into Docker Hub"
 docker login -u peco602
+
+echo " [+] Tagging image"
 docker tag ssh-server:latest peco602/ssh-linux-docker:latest
+
+echo " [+] Pushing to Docker Hub"
 docker push peco602/ssh-linux-docker:latest
